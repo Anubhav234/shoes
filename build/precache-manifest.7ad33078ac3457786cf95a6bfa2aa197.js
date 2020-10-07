8d0b9f56f2daf02113703136982d4f992abaef87
anubhav singh <abvsingh333@gmail.com>
2020-10-07 17:53:14 +0530

    initial

---
+++ build/precache-manifest.7ad33078ac3457786cf95a6bfa2aa197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf4a49f7ef0943683f34",
+    "url": "/static/css/main.798bb7c5.chunk.css"
+  },
+  {
+    "revision": "bf4a49f7ef0943683f34",
+    "url": "/static/js/main.198f1bce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0b305ce16e13c0dcfd3",
+    "url": "/static/css/2.ca42fe58.chunk.css"
+  },
+  {
+    "revision": "a0b305ce16e13c0dcfd3",
+    "url": "/static/js/2.403cfed7.chunk.js"
+  },
+  {
+    "revision": "494ef9a617fd725f32901977665559a4",
+    "url": "/static/media/logo.494ef9a6.svg"
+  },
+  {
+    "revision": "867e94254575b4bc12c79133f85fc544",
+    "url": "/index.html"
+  }
+];
